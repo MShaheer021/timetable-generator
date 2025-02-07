576149051af11ef56e8269030b97a62cf9390537
MShaheer021 <muhammadshaheer2002@gmail.com>
2025-02-07 06:49:58 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import ForgotPassword from './components/ForgotPassword';
-import Home from './components/Home'
+import Home from './components/Home';
 import './App.css';
 
 const App = () => {
     return (
         <div className='main'>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<HomeWithButton />} /> {/* Default route */}
+                </Routes>
+            </Router>
+        </div>
+    );
+};
 
-        <Router>
-            <Routes>
-                <Route path="/Login" element={<Login />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/home" element={<Home/>} />
-            </Routes>
-        </Router>
+const HomeWithButton = () => {
+    const navigate = useNavigate();
 
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
+    return (
+        <div className='center'>
+            <h1>Welcome to the Time Table Generator</h1>
+            <button onClick={handleLoginClick}>Go to Login</button>
         </div>
     );
 };
